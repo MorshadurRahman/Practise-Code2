4e0d062f3233d5ecf26111a511cbfc253bccd961
MorshadurRahman <morshadurrahman5@gmail.com>
2025-08-14 21:40:22 +0600

    Add files via upload

---
+++ Multiplication Table.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, m;
+
+    cout << "enter a number : " ;
+    cin >>n;
+
+    for(m=1; m <= 12; m++)
+    {
+        cout << m << "*" << n << "=" << (n*m) << endl;
+    }
+
+        return 0;
+}
