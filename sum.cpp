9532fa6a58c9b5f042977a0f7e6cd8ced80beb2d
MorshadurRahman <morshadurrahman5@gmail.com>
2025-08-15 02:35:35 +0600

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,3 +1,5 @@
+//Write a program that asks the user for a number n and prints the sum of the numbers 1 to n
+
 #include<iostream>
 using namespace std;
 int main()
@@ -15,3 +17,4 @@ int main()
     cout << "The sum of the numbers 1 to " << n << endl;
     return 0;
 }
+
