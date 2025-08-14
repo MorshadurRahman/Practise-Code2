b5d8085ef549c3701032c35501a94853ca53f245
MorshadurRahman <morshadurrahman5@gmail.com>
2025-08-15 02:34:03 +0600

    Update Name and greet.cpp

--- Name and greet.cpp
+++ Name and greet.cpp
@@ -1,3 +1,5 @@
+//Write a program that asks the user for their name and greets them with their name
+
 #include<iostream>
 using namespace std;
 int main()
@@ -9,3 +11,4 @@ int main()
    cout <<"Hello, " << name;
    return 0;
 }
+
